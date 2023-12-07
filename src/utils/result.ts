9b4c2c1cfab227b79fccddd758c6bda456d83a2b
zhantan2015 <zhantan2015@gmail.com>
2023-12-07 16:41:51 +0000

    添加响应对象

---
+++ src/utils/result.ts
@@ -0,0 +1,48 @@
+
+import { Response } from 'express'
+
+type result_code = 200 | 400 | 500
+type result_data = {} | [] | undefined
+
+export default class {
+    res: Response
+    body: {
+        code: result_code
+        msg: string
+        data?: {} | []
+    }
+
+    constructor(res: Response) {
+        this.res = res
+        this.body = {
+            code: 200,
+            msg: "",
+        }
+    }
+
+    success(msg: string) {
+        this.body.code = 200
+        this.body.msg = msg
+        return this
+    }
+
+    failed(msg: string) {
+        this.body.code = 400
+        this.body.msg = msg
+        return this
+    }
+
+    error(msg: string) {
+        this.body.code = 500
+        this.body.msg = msg
+        return this
+    }
+
+    setData(data: result_data) {
+        this.body.data = data
+    }
+
+    send() {
+        this.res.send(this.body)
+    }
+}
